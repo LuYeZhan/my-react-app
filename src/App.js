adabaad609aedbf7c9bb1ceb25e4cfd2b92eea62
Ye Zhan <lu@hooli.store>
2020-06-10 14:21:03 +0200

    using the useState() Hook for state manipulation

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-class App extends Component {
-  state = {
+const app = (props) => {
+  const [personsState, setPersonsState] = useState({
     persons: [
       { name: 'Pepe', age: 28 },
       { name: 'Pepa', age: 29 },
       { name: 'Pepo', age: 31 },
     ],
-    otherState: 'someother value',
-  };
+  });
 
-  // switchNameHandler name example of method naming
-  switchNameHandler = () => {
+  const [otherState, setOtherState] = useState('some other value');
+
+  console.log(personsState, otherState);
+
+  const switchNameHandler = () => {
     // console.log('was clicked!');
     // this.state.persons[0].name = 'pepehands'; Dont do this we shoild use this.setState
-    this.setState({
+    setPersonsState({
       persons: [
         { name: 'Pepeeeee', age: 28 },
         { name: 'Pepa', age: 29 },
@@ -25,34 +27,34 @@ class App extends Component {
     });
   };
 
-  render() {
-    return (
-      <div className='App'>
-        <h1>Hi, I am a react app!</h1>
-        <p>This is really working!</p>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
-        <Person
-          name={this.state.persons[0].name}
-          age={this.state.persons[0].age}
-        />
-        <Person
-          name={this.state.persons[1].name}
-          age={this.state.persons[1].age}
-        >
-          My hobbies: are pepehands
-        </Person>
-        <Person
-          name={this.state.persons[2].name}
-          age={this.state.persons[2].age}
-        />
-      </div>
-    );
-    // return React.createElement(
-    //   'div',
-    //   { className: 'App' },
-    //   React.createElement('h1', null, 'Does this work now?')
-    // );
-  }
-}
+  return (
+    <div className='App'>
+      <h1>Hi, I am a react app!</h1>
+      <p>This is really working!</p>
+      <button onClick={switchNameHandler}>Switch Name</button>
+      <Person
+        name={personsState.persons[0].name}
+        age={personsState.persons[0].age}
+      />
+      <Person
+        name={personsState.persons[1].name}
+        age={personsState.persons[1].age}
+      >
+        My hobbies: are pepehands
+      </Person>
+      <Person
+        name={personsState.persons[2].name}
+        age={personsState.persons[2].age}
+      />
+    </div>
+  );
+  // return React.createElement(
+  //   'div',
+  //   { className: 'App' },
+  //   React.createElement('h1', null, 'Does this work now?')
+  // );
+};
 
-export default App;
+export default app;
+
+// switchNameHandler name example of method naming

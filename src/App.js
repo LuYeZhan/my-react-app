4e81568ef3440d69ade1f0bbb5733cce75b2397b
Ye Zhan <lu@hooli.store>
2020-06-10 10:38:27 +0200

    functional components and props usage

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className='App'>
+        <h1>Hi, I am a react app!</h1>
+        <p>This is really working!</p>
+        <Person name='Pepe' age='30' />
+        <Person name='pepa' age='29'>
+          My hobbies: are pepehands
+        </Person>
+        <Person name='Pepo' age='31' />
       </div>
     );
+    // return React.createElement(
+    //   'div',
+    //   { className: 'App' },
+    //   React.createElement('h1', null, 'Does this work now?')
+    // );
   }
 }
 

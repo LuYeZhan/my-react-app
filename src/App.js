1a84d0f25179af74f23304a50b30c1d74a3d8ea7
Ye Zhan <lu@hooli.store>
2020-06-10 13:44:14 +0200

    handling events with methods

--- src/App.js
+++ src/App.js
@@ -3,16 +3,39 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: 'Pepe', age: 28 },
+      { name: 'Pepa', age: 29 },
+      { name: 'Pepo', age: 31 },
+    ],
+  };
+
+  // switchNameHandler name example of method naming
+  switchNameHandler = () => {
+    console.log('was clicked!');
+  };
+
   render() {
     return (
       <div className='App'>
         <h1>Hi, I am a react app!</h1>
         <p>This is really working!</p>
-        <Person name='Pepe' age='30' />
-        <Person name='pepa' age='29'>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}
+        />
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+        >
           My hobbies: are pepehands
         </Person>
-        <Person name='Pepo' age='31' />
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age}
+        />
       </div>
     );
     // return React.createElement(

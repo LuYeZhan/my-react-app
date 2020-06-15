a9d4a6b37049359b448d4b9c10e0efc72b5beeae
Ye Zhan <lu@hooli.store>
2020-06-15 11:24:16 +0200

    rendering flexible lists using a shadow copy of the index

--- src/App.js
+++ src/App.js
@@ -34,13 +34,23 @@ class App extends Component {
     this.setState({ persons });
   };
 
-  nameChangehandler = (event) => {
+  nameChangehandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex((p) => {
+      return p.id === id;
+    });
+
+    const person = { ...this.state.persons[personIndex] };
+
+    // alternative way of doing it
+    // const person = Object.assign({}, this.state.persons[personIndex])
+
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
     this.setState({
-      persons: [
-        { name: 'Max', age: 28 },
-        { name: event.target.value, age: 29 },
-        { name: 'Stephanie', age: 26 },
-      ],
+      persons,
     });
   };
 
@@ -69,6 +79,7 @@ class App extends Component {
                 name={person.name}
                 age={person.age}
                 key={person.id}
+                changed={(event) => this.nameChangehandler(event, person.id)}
               />
             );
           })}

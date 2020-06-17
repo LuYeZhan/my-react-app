bf06aaf4676d057c5b1b60e32342ef412fdb1809
Ye Zhan <lu@hooli.store>
2020-06-17 12:01:42 +0200

    rendering adjacent jsx elements with hoc aux trick

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -48,8 +48,8 @@ class Persons extends PureComponent {
           click={() => this.props.clicked(index)}
           name={person.name}
           age={person.age}
-          changed={(event) => this.props.changed(event, person.id)}
           key={person.id}
+          changed={(event) => this.props.changed(event, person.id)}
         />
       );
     });
